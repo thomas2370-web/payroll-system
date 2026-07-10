6738a2ade04ee531dd4b568d05940ba4fefd0920
Your Name <you@example.com>
2026-07-10 21:55:51 +0100

    Seed demo accounts on app startup

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,20 @@
+import os
+
 from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
+
+    def ready(self):
+        if os.environ.get("RUN_MAIN") != "true":
+            return
+        from django.conf import settings
+
+        if settings.DEBUG:
+            return
+
+        from django.core.management import call_command
+
+        call_command("seed_demo_data", verbosity=0)
